642614ffaefa185b60ce240531ad3ec7c6b28902
ram-chadar <javaprojectwork2715@gmail.com>
2024-03-27 12:49:12 +0530

    initial

---
+++ src/com/jbk/Test.java
@@ -0,0 +1,13 @@
+package com.jbk;
+
+import com.jbk.operation.Operation;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Operation operation=new Operation();
+		operation.add(10, 20);
+
+	}
+
+}
---
+++ src/com/jbk/operation/Operation.java
@@ -0,0 +1,13 @@
+package com.jbk.operation;
+
+public class Operation {
+	
+	public void add(int a,int b) {
+		
+		int c=a+b;
+		System.out.println("Addition : "+c);
+	}
+	
+	
+
+}
